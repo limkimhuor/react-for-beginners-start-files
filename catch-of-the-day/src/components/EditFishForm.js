9b626ea08b8610e1234e8f674799ac930c42f3ef
limkimhuor <kimhuorlim@gmail.com>
2018-06-13 15:41:09 +0700

    Bi-directional data flow and live state editing

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class EditFishForm extends Component {
+  handleChange = event => {
+    // update that fish
+    // 1. Take a copy of the current fish
+    const updateFish = {
+      ...this.props.fish,
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    this.props.updateFish(this.props.index, updateFish);
+  };
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          name="name"
+          type="text"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          name="price"
+          type="text"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select
+          name="status"
+          type="text"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea
+          name="desc"
+          type="text"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          name="image"
+          type="text"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
